2007099dccf51ebffa2000796e10d26e99508a3d
Alex <awarrington0895@gmail.com>
2022-06-22 18:22:43 +0000

    Give inventory a fixed size

--- src/inventory.spec.ts
+++ src/inventory.spec.ts
@@ -1,12 +1,23 @@
 import { describe, it, expect } from 'vitest';
 import * as Inventory from './inventory';
+import * as E from 'fp-ts/Either';
+import { pipe } from 'fp-ts/function';
 
 describe('Inventory', () => {
     it('should be able to be added to', () => {
-        const inventory = Inventory.empty();
+        const inventory = Inventory.empty(1);
 
-        const updatedInventory = Inventory.add('morphine', inventory);
-
+        const updatedInventory = pipe(
+            Inventory.add('morphine', inventory),
+            E.getOrElse(() => Inventory.empty(0))
+        );
+        
         expect(Inventory.contains('morphine', updatedInventory)).toBe(true);
+    });
+
+    it('should have a fixed size', () => {
+        const inventory = Inventory.empty(0);    
+
+        expect(Inventory.add('morphine', inventory)).toStrictEqual(E.left(new Error('Inventory is full')));
     });
 });
--- src/inventory.ts
+++ src/inventory.ts
@@ -1,9 +1,28 @@
-const empty = () => ([]);
+import * as E from 'fp-ts/Either';
 
-const add = (item: string, inventory: string[]): string[] => ([...inventory, item ]);
+type Inventory = {
+    size: number;
+    items: string[];
+}
+
+const empty = (size: number): Inventory => ({
+    size,
+    items: []
+});
+
+const add = (item: string, inventory: Inventory): E.Either<Error, Inventory> => {
+    if (inventory.items.length >= inventory.size) {
+        return E.left(new Error('Inventory is full'));
+    }
+
+    return E.right({
+        size: inventory.size,
+        items: inventory.items.concat(item)
+    });
+};
     
 
-const contains = (item: string, inventory: string[]): boolean => inventory.includes(item);
+const contains = (item: string, inventory: Inventory): boolean => inventory.items.includes(item);
 
 
-export { empty, add, contains };
+export { empty, add, contains, Inventory as Type };
